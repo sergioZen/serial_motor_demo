fb9d371001b9cb7235f131108a2c2a8c91f0a49e
sergioZen <ecologicalbrasil@gmail.com>
2023-03-21 18:50:12 +0100

    Fixed: ROS1 conversion node

--- serial_motor_demo/src/driver.py
+++ serial_motor_demo/src/driver.py
@@ -18,11 +18,11 @@ class MotorDriver():
         self.loop_rate = int(rospy.get_param("~loop_rate", 0))
         self.serial_port = rospy.get_param("~serial_port", "/dev/ttyUSB0")
         self.baud_rate = rospy.get_param("~baud_rate", 57600)
-        self.serial_debug = rospy.get_param("~serial_debug", False)
+        self.debug_serial_cmds = rospy.get_param("~serial_debug", False)
 
         # Overall loop rate: should be faster than fastest sensor rate
         self.rate = int(rospy.get_param("~rate", 50))
-        r = rospy.Rate(self.rate)
+        rospy.Rate(self.rate)
 
         # Setup topics & services
         
@@ -70,7 +70,7 @@ class MotorDriver():
             self.send_pwm_motor_command(motor_command.mot_1_req_rad_sec, motor_command.mot_2_req_rad_sec)
         else:
             # counts per loop = req rads/sec X revs/rad X counts/rev X secs/loop 
-            scaler = (1 / (2*math.pi)) * self.get_parameter('encoder_cpr').value * (1 / self.get_parameter('loop_rate').value)
+            scaler = (1 / (2*math.pi)) * self.encoder_cpr * (1 / self.encoder_cpr)
             mot1_ct_per_loop = motor_command.mot_1_req_rad_sec * scaler
             mot2_ct_per_loop = motor_command.mot_2_req_rad_sec * scaler
             self.send_feedback_motor_command(mot1_ct_per_loop, mot2_ct_per_loop)
@@ -88,7 +88,7 @@ class MotorDriver():
             m2_diff = resp[1] - self.last_m2_enc
             self.last_m2_enc = resp[1]
 
-            rads_per_ct = 2*math.pi/self.get_parameter('encoder_cpr').value
+            rads_per_ct = 2*math.pi/self.encoder_cpr
             self.m1_spd = m1_diff*rads_per_ct/time_diff
             self.m2_spd = m2_diff*rads_per_ct/time_diff
 
@@ -151,9 +151,10 @@ if __name__ == '__main__':
     
     ###### main loop  ######
     while not rospy.is_shutdown():
-      rospy.spinOnce(motor_driver)
       motor_driver.check_encoders()
-        
+      
+      # rospy.spin()
+
       idle.sleep()    
 
     motor_driver.close_conn()
